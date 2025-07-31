067dfdf048a7ef24122a21213260878557f2d004
likeskymore <trung7p@gmail.com>
2025-07-31 14:06:54 +0700

    stripe integration

--- app/(root)/order/[id]/order-details-table.tsx
+++ app/(root)/order/[id]/order-details-table.tsx
@@ -28,15 +28,18 @@ import {
 } from "@/lib/actions/order.actions";
 import { useTransition } from "react";
 import { toast } from "sonner";
+import StripePayment from "./stripe-payment";
 
 const OrderDetailsTable = ({
   order,
   paypalClientId,
   isAdmin,
+  stripeClientSecret,
 }: {
   order: Order;
   paypalClientId: string;
   isAdmin: boolean;
+  stripeClientSecret: string | null;
 }) => {
   const {
     shippingAddress,
@@ -106,7 +109,7 @@ const OrderDetailsTable = ({
     );
   };
 
-    const MarkAsDeliveredButton = () => {
+  const MarkAsDeliveredButton = () => {
     const [isPending, startTransition] = useTransition();
 
     return (
@@ -128,7 +131,6 @@ const OrderDetailsTable = ({
       </Button>
     );
   };
-
 
   return (
     <>
@@ -237,6 +239,16 @@ const OrderDetailsTable = ({
                   </PayPalScriptProvider>
                 </div>
               )}
+
+              {/* Stripe */}
+              {!isPaid && paymentMethod === "Stripe" && stripeClientSecret && (
+                <StripePayment
+                  priceInCents={Number(order.totalPrice) * 100}
+                  orderId={order.id}
+                  clientSecret={stripeClientSecret}
+                />
+              )}
+
               {/* COD */}
               {isAdmin && !isPaid && paymentMethod === "CashOnDelivery" && (
                 <MarkAsPaidButton />
--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -4,32 +4,49 @@ import { notFound } from "next/navigation";
 import OrderDetailsTable from "./order-details-table";
 import { ShippingAddress } from "@/types";
 import { auth } from "@/auth";
-
+import Stripe from "stripe";
 
 export const metadata: Metadata = {
-    title: 'Order Details',
-}
+  title: "Order Details",
+};
 
 const OrderDetailsPage = async (props: {
-    params: Promise<{
-        id: string
-    }>
+  params: Promise<{
+    id: string;
+  }>;
 }) => {
-    const {id} = await props.params;
+  const { id } = await props.params;
 
-    const order  = await getOrderById(id)
+  const order = await getOrderById(id);
 
-    if(!order) notFound();
+  if (!order) notFound();
 
-    const session = await auth()
+  const session = await auth();
 
-    return ( <OrderDetailsTable order={{
+  let client_secret = null;
+
+  if (order.paymentMethod === "Stripe" && !order.isPaid) {
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Math.round(Number(order.totalPrice) * 100),
+      currency: "USD",
+      metadata: { orderId: order.id },
+    });
+    client_secret = paymentIntent.client_secret;
+  }
+
+  return (
+    <OrderDetailsTable
+      order={{
         ...order,
-        shippingAddress: order.shippingAddress as ShippingAddress
-    }}
-    paypalClientId={process.env.PAYPAL_CLIENT_ID ||'sb'} 
-    isAdmin = {session?.user?.role === 'admin' || false} /> );
-    
-}
- 
+        shippingAddress: order.shippingAddress as ShippingAddress,
+      }}
+      stripeClientSecret={client_secret}
+      paypalClientId={process.env.PAYPAL_CLIENT_ID || "sb"}
+      isAdmin={session?.user?.role === "admin" || false}
+    />
+  );
+};
+
 export default OrderDetailsPage;
